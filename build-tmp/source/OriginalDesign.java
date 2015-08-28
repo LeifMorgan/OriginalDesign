6de793f04be900467dd6187f9aa567e86664989a
LeifMorgan <leif.e.morgan@gmail.com>
2015-08-28 10:04:55 -0700

    add first name to index

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,8 +1,24 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 {
 		size(400, 400);
 }
-void draw()
+public void draw()
 {
 	background(127);
 	//var
@@ -114,9 +130,18 @@ void draw()
 
 
 }
-void mousePressed()
+public void mousePressed()
 {
 	
 
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
